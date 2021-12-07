cfe4fcede4097c7b607b91a45d04cda8d49a9617
Rukundo Kevin <kevinrukundo1@gmail.com>
2021-12-07 19:47:40 +0200

    Tried making title unscrollable

--- script/js.js
+++ script/js.js
@@ -1,6 +1,11 @@
 let title = document.querySelector('.title');
-let color = ['#2350f8','#0d8476','#fd9175','#133f81','#7931a5','coral','#a3101e','#a3101e','#1c59f7']
-  console.log(Math.round(Math.random() * 10))
+let color = ['#2350f8','#0d8476','#fd9175','#133f81','#7931a5','coral','#a3101e','#a3101e','#1c59f7','#607662']
+
+
 title.addEventListener("click",function (){
     title.style.color = color[Math.round(Math.random() * 10)];
+})
+
+document.addEventListener("scroll",function(){
+    title.classList.add('dark');
 })
